c669b52970d32c42512207b068dec3027d2dc1f8
Gus Vroustouris <gvrousto@purdue.edu>
2017-12-03 14:37:06 -0500

    added avi's initial unity template

---
+++ Unity Assets/Assets/Scripts/joinGame_click.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class joinGame_click : MonoBehaviour {
+    public GameObject ws;
+	// Use this for initialization
+	void Start () {
+        //GameObject waitS = GameObject.FindGameObjectWithTag("waitScreen");
+        //waitS.SetActive(true);
+
+    }
+	
+	// Update is called once per frame
+	public void butpress () {
+        //GameObject waitS = GameObject.FindGameObjectWithTag("waitScreen");
+        ws.SetActive(true);
+
+    }
+}
---
+++ Unity Assets/Assets/Scripts/wordDef.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class wordDef : MonoBehaviour {
+
+    public Text timer;
+    public GameObject defSelect;
+    float time;
+
+	// Use this for initialization
+	void Start () {
+        time = 15;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time -= Time.deltaTime;
+        if(time < 0)
+        {
+            time = 0;
+            defselect();
+        }
+        int rtme = (int)time;
+        timer.text = "Time: " + rtme + " s";
+	}
+
+    void defselect()
+    {
+        GameObject wdf = GameObject.FindGameObjectWithTag("wordDef");
+        wdf.SetActive(false);
+        defSelect.SetActive(true);
+    }
+}
