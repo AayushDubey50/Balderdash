1706920c5ebc360f309e2f9064b8c0f7bf38388b
Aayush Dubey <aayush.dubey50@gmail.com>
2017-12-03 17:48:55 -0500

    Update gameFunctionCall.php
    Re-updated file according to game_action functions' return values.

--- php/gameFunctionCall.php
+++ php/gameFunctionCall.php
@@ -8,30 +8,38 @@
             $join_game = $game_action->join_game($_SESSION["userID"]);
             if ($join_game[1]) {
                 // $_SESSION["word"] should be the selected word
-                // $onStart should be an array with elements of the format: [username, points]
+                // $onStart should be an array with username as the key and username's points as the value
                 $onStart = $game_action->onStart($_SESSION["gameID"]);
-            } else if ($join_game[0]) ; // should start countdown of players joining a game
+            } else if ($join_game[0]) {
+                $_SESSION["loadingCountdown"] = time();
+            }
+            //; // should start countdown of players joining a game
             // $join_game[2] should be an array of the usernames
             break;
         case "onStart":
+            if (isset($_SESSION["loadingCountdown"])) unset($_SESSION["loadingCountdown"]);
             // $_SESSION["word"] should be the selected word
-            // $onStart should be an array with elements of the format: [username, points]
+            // $onStart should be an array with username as the key and username's points as the value
             $onStart = $game_action->onStart($_SESSION["gameID"]);
             break;
-        case "inputDefinitions":
-            $inputDefinitions = $game_action->inputDefinitions($_SESSION["gameID"], $_SESSION["userID"], $_POST["input"]);
-            if ($inputDefinitions) $wordsAndDefin = $game_action->wordsAndDefin($_SESSION["gameID"]);
-            // 
+        case "input_definition":
+            $input_definition = $game_action->input_definition($_SESSION["gameID"], $_SESSION["userID"], $_POST["input"]);
+            if ($input_definition == NULL) ; // Have user submit another definition since it's already taken
+            if ($input_definition) $onChoices = $game_action->onChoices($_SESSION["gameID"]);
+            // $onChoices should be an array of string definitions
+            // $_SESSION["idsDefs"] should be an array with elements of the format: [userID, definition]. userID = 0 means the computer
             break;
-        case "wordsAndDefin":
-            $wordsAndDefin = $game_action->wordsAndDefin($_SESSION["gameID"]);
+        case "onChoices":
+            $onChoices = $game_action->onChoices($_SESSION["gameID"]);
+            // $onChoices should be an array of string definitions
+            // $_SESSION["idsDefs"] should be an array with elements of the format: [userID, definition]. userID = 0 means the computer
             break;
         case "select_definition":
             $select_definition = $game_action->select_definition($_SESSION["gameID"], $_SESSION["userID"], $_POST["selectionID"]);
-            if ($select_definition) $round_summary = $game_action->round_summary($_SESSION["gameID"]);
+            if ($select_definition) $onSummary = $game_action->onSummary($_SESSION["gameID"]);
             break;
-        case "round_summary":
-            $round_summary = $game_action->round_summary($_SESSION["gameID"]);
+        case "onSummary":
+            $onSummary = $game_action->onSummary($_SESSION["gameID"]);
             break;
     }
 ?>
