25e7304e758c8969fffbed9dab83fcd07ccb8f6e
Aayush Dubey <aayush.dubey50@gmail.com>
2017-12-06 16:13:18 -0500

    Update gameFunctionCall.php

--- php/gameFunctionCall.php
+++ php/gameFunctionCall.php
@@ -10,11 +10,11 @@
             case "join_game": // user presses "Join Game" button
                 $join_game = $game_action->join_game($_SESSION["userID"]);
                 if ($join_game[1]) $toDump = $game_action->onStart($_SESSION["gameID"]);
-                else $toDump = $join_game[0];
+                else $toDump = strval($join_game[0]);
                 break;
             case "onStart":
-                // $_SESSION["word"] == $onStart[0] should be the selected word
-                // $onStart[1] should be an array with username as the key and username's points as the value
+                // $_SESSION["word"] == $toDump[0] should be the selected word
+                // $toDump[1] should be an array with username as the key and username's points as the value
                 $toDump = $game_action->onStart($_SESSION["gameID"]);
                 break;
             case "input_definition": // user submits a definition
@@ -25,12 +25,11 @@
                 else $toDump = "Your definition has been submitted.";
                 break;
             case "onChoices":
-                $toDump = $game_action->onChoices($_SESSION["gameID"]); // $onChoices should be an array of string definitions
+                $toDump = $game_action->onChoices($_SESSION["gameID"]); // $toDump should be an array of string definitions
                 // $_SESSION["idDefs"] gets set to be an array with elements of the format: ["definition" => userID]. userID = 0 means the Computer
                 break;
             case "select_definition": // user votes a user's definition
-                $selectionDef = $_POST["functionName"][1];
-                $selectionID = $_SESSION["idDefs"][$selectionDef];
+                $selectionID = $_SESSION["idDefs"][$_POST["functionName"][1]];
                 $select_definition = $game_action->select_definition($_SESSION["gameID"], $_SESSION["userID"], $selectionID);
                 if ($select_definition) $toDump = $game_action->onSummary($_SESSION["gameID"]);
                 else $toDump = "Your vote has been submitted.";
@@ -39,10 +38,10 @@
                 $toDump = $game_action->onSummary($_SESSION["gameID"]);
                 break;
             case "reset_round":
-                $game_action->reset_round($_SESSION["gameID"], $_SESSION["userID"]);
+                $game_action->reset_round($_SESSION["gameID"]);
                 break;
             case "onEnd":
-                $onEnd = $game_action->onEnd($_SESSION["gameID"]);
+                $toDump = $game_action->onEnd($_SESSION["gameID"], $_SESSION["userID"]);
                 break;
         }
         if (is_array($toDump)) print_r($toDump);
