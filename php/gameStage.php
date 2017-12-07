a85103c5c6b0ddc37520cf8708ce9a7cc867b6d6
Aayush Dubey <aayush.dubey50@gmail.com>
2017-12-07 06:01:43 -0500

    Update gameStage.php

--- php/gameStage.php
+++ php/gameStage.php
@@ -1,38 +1,36 @@
 <?php
+    //session_start();
     require_once("game/game_action.php");
     $game_action = new game_action();
 
-    if ($_POST && isset($_POST["gameStage"])) {
-        $gameStage = $_POST["gameStage"];
-        $toDump = array();
-        switch ($gameStage) {
-            case "gameStage": // $gameStage returns True if another player can join the game
-                $getStageID = $game_action->getStageID($getStageID);
-                switch ($getStageID) {
-                    case 0:
-                        $page = "loadingPage";
-                        $getUsernames = $game_action->getUsernames($getStageID);
-                        array_push($toDump, $page, $getUsernames);
-                        break;
-                    case 1:
-                        $page = "guessingPage";
-                        $getNum = $game_action->getNum($getStageID, "userIDsDef", "|");
-                        array_push($toDump, $page, $getNum);
-                        break;
-                    case 2:
-                        $page = "votingPage";
-                        $getNum = $game_action->getNum($getStageID, "selectionIDs", "|");
-                        array_push($toDump, $page, $getNum);
-                        break;
-                    case 3:
-                        array_push($toDump, "roundPage");
-                        break;
-                    case 4:
-                        array_push($toDump, "endPage");
-                        break;
-                }
+    if ($_POST && isset($_POST["callStage"])) {
+        $toDump = "";
+        $gameID = intval($_POST["callStage"]);
+        $getStageID = $game_action->getStageID($gameID);
+        switch ($getStageID) {
+            case 0:
+                $getUsernames = $game_action->getUsernames($gameID);
+                $startTimer = $game_action->startTimer($gameID);
+                $toDump = $startTimer."\n";
+                $toDump .= implode("\n", $getUsernames);
+                echo $toDump;
+                break;
+            case 1:
+                $toDump = $game_action->getNum($gameID, "userIDsDef", "|");
+                echo $toDump;
+                break;
+            case 2:
+                $toDump = $game_action->getNum($gameID, "selectionIDs", "|");
+                echo $toDump;
+                break;
+            case 3:
+                //array_push($toDump);
+                print_r($toDump);
+                break;
+            case 4:
+                //array_push($toDump);
+                print_r($toDump);
                 break;
         }
-        print_r($toDump);
     }
 ?>
